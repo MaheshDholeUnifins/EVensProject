bd5f1344200e537f99d6328463b6ff4105b37fe5
MAHESH DHOLE <126243092+MAHESHDHOLE1999@users.noreply.github.com>
2024-07-29 13:03:37 +0530

    Update AboutUs.jsx

--- src/Screen/AboutUs.jsx
+++ src/Screen/AboutUs.jsx
@@ -7,15 +7,15 @@ import { useNavigate } from 'react-router-dom';
 const AboutUs = () => {
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(!localStorage.getItem('authToken')){
-      navigate('/login');
-    }
-  },[navigate])
+  // useEffect(()=>{
+  //   if(!localStorage.getItem('authToken')){
+  //     navigate('/login');
+  //   }
+  // },[navigate])
 
-  if(!localStorage.getItem('authToken')){
-    return null;;
-  }
+  // if(!localStorage.getItem('authToken')){
+  //   return null;
+  // }
   return (
     <div>
         <div className='w-full h-60 flex justify-center items-center lg:w-full lg:h-[722px] bg-cover bg-center md:justify-center md:items-center lg:text-5xl font-bold text-white'  style={{backgroundImage : `url(${AboutUsImg})`}}>
